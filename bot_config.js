def515a0486c9ed75087784c25691a238e2df84a
NwiZ <152017753+NirmataNetwork@users.noreply.github.com>
2024-05-19 19:52:46 +0300

    add

---
+++ bot_config.js
@@ -0,0 +1,23 @@
+module.exports = { 
+
+    wallethostname: "",
+    daemonhostname: "",
+    bot_token : "",
+    mongodburl: "",
+    
+    owner_id_1 : "",
+    owner_id_2 : "",
+    coin_name : "NiR",
+    block_maturity_requirement : 10,
+    coin_total_units : 12,
+    coin_display_units : 2,
+    server_wallet_address : "",
+    withdraw_tx_fees : 0.001618,
+    withdraw_min_amount: "1",
+    wait_time_for_withdraw_confirm : 30000,
+    custom_message_length_limit: 100,
+    default_fee: 1618000000,
+    log_1 : true,
+    log_2 : true,
+    log_3 : true
+};
